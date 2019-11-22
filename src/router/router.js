5e17192c679b20303c6a8c1f000c7d7e45ac7e65
autumnFish <517729329@qq.com>
2019-11-22 11:15:59 +0800

    首页模块 - 整体布局

--- src/router/router.js
+++ src/router/router.js
@@ -1,27 +1,33 @@
 // 导入vue-router
-import VueRouter from 'vue-router'
+import VueRouter from "vue-router";
 // 导入vue 用就要导入
-import Vue from 'vue'
+import Vue from "vue";
 // use
-Vue.use(VueRouter)
-
+Vue.use(VueRouter);
 
 // 导入 登录页面组件
-import login from '../views/login/login.vue'
+import login from "../views/login/login.vue";
+// 导入 首页页面组件
+import index from "../views/index/index.vue";
 
 // 规则
 const routes = [
-    // 登录组件规则
-    {
-        path:"/login",
-        component:login
-    }
-]
+  // 登录组件规则
+  {
+    path: "/login",
+    component: login
+  },
+  // 首页组件规则
+  {
+    path: "/index",
+    component: index
+  }
+];
 
 // 创建路由示例
 const router = new VueRouter({
-    routes
-})
+  routes
+});
 
 // 挂载到Vue实例上
 // 暴露出去
